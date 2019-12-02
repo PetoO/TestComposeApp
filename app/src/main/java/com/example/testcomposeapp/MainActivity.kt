c101cd8b23acd14b78033a6607b5b9005afc6c4f
Peter Zapalač <peter.zapalac@eset.sk>
2019-12-02 16:00:11 +0100

    updates from work

--- app/src/main/java/com/example/testcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/testcomposeapp/MainActivity.kt
@@ -4,30 +4,44 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.foundation.shape.border.Border
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            DefaultPreview()
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Spacing(24.dp)  )
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        Greeting("Android")
+        Column() {
+            Surface(color = Color.Cyan, border = Border(color = Color.Red, width = 2.dp) ) {
+                Greeting("Android")
+            }
+            Surface(color = Color.Cyan, border = Border(color = Color.Red, width = 2.dp) ) {
+                Greeting("Android")
+            }
+            Surface(color = Color.Cyan, border = Border(color = Color.Red, width = 2.dp) ) {
+                Greeting("Android")
+            }
+        }
     }
 }
