f9024ffde9ea42da573a17ab20bb79dda7789523
Peter Zapalac <zapalac.peter@gmail.com>
2019-12-02 22:41:08 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/example/testcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/testcomposeapp/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.testcomposeapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.dp
+import androidx.compose.Model
 import androidx.ui.core.setContent
-import androidx.ui.foundation.shape.border.Border
-import androidx.ui.graphics.Color
-import androidx.ui.layout.Column
-import androidx.ui.layout.Spacing
+import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
+
+@Model
+class CounterState(var count: Int = 0)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,25 +21,20 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Spacing(24.dp)  )
-}
-
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Column() {
-            Surface(color = Color.Cyan, border = Border(color = Color.Red, width = 2.dp) ) {
-                Greeting("Android")
-            }
-            Surface(color = Color.Cyan, border = Border(color = Color.Red, width = 2.dp) ) {
-                Greeting("Android")
-            }
-            Surface(color = Color.Cyan, border = Border(color = Color.Red, width = 2.dp) ) {
-                Greeting("Android")
-            }
-        }
+    MaterialTheme() {
+        DisplayButtonCounter()
     }
+}
+
+@Composable
+fun DisplayButtonCounter(state: CounterState = CounterState()) {
+    Button(
+        text = "I've been clicked ${state.count} times",
+        onClick = {
+            state.count++
+        }
+    )
 }
